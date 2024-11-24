76ccad07a01a99e28a3235a0dfca49e933d935f9
ChristOurLife <catg184@duck.com>
2024-11-24 18:48:51 -0500

    Fixed more bugs and added a scary debug mode
    pleasebringbackthekids

--- Program Groups.bat
+++ Program Groups.bat
@@ -10,33 +10,47 @@ if not exist "%agreement%" (
     echo Please note this code may break at any time, and the author of this code will probably not bother to fix it. By proceeding, you agree to sign your life away to some random guy online.
     pause
     echo TOS has been signed! > "%agreement%"
+    
 )
 
 :launchProgramGroup
+cls
 echo Which set of programs would you like to launch? You've saved:
 Rem Does the file exist? Breathe if yes. Recite the bible in Japanese if no.
-if not exist "%groupFile%" >nul (
-    echo "Oops! You haven't saved any program groups yet. Would you like to create some?"
+if not exist "%groupFile%"  (
+    echo Oops! You haven't saved any program groups yet. Would you like to create some?
     set /p CreateProgramPrompt="(Y/N)"
-    if /i "%CreateProgramPrompt%"=="Y" (goto CreateProgramGroup) else if /i "%CreateProgramPrompt%"=="N" (curl parrot.live)
+    if "%CreateProgramPrompt%"=="Y" (goto CreateProgramGroup) else if /i "%CreateProgramPrompt%"=="N" (curl parrot.live) else if /i "%CreateProgramPrompt%"="debug" (goto debug)
 ) else (
     echo Program groups found:
     type %groupFile%
     set /p programGroupChoice=Which program group would you like to launch?
+    if %programGroupChoice%==debug (goto debug)
 )
 
 :CreateProgramGroup
 find "creatorGroupProgramVisited=1" "%memory%" >nul
 if %errorlevel%==1 (
     timeout /t 3 >nul
-    echo Welcome to the Program Group Creator®!
+    echo Welcome to the Program Group Creator!
     echo creatorGroupProgramVisited=1 >> "%memory%"
-) else (
-    echo .
-)
+) else (echo .)
 
 echo What is the name of the program group you would like to create?
 set /p programGroupName=Format: "Example Group"
-echo "%programGroupName%">> "%groupFile%"
+set /p programPaths=What are the paths to your programs? {Format: C:\\Users\ExampleUser\ExampleDesktop\ExampleYadaYadaYada\pleasebringthekidsback.exe}
+Rem Create files needed for later
+echo "%programGroupName%" >> "%groupFile%"
+echo "%programPaths%" "%programGroupName%"
+
 set /p createrAgain=Would you like to create another program group? (Y/N)
 if /i "%createrAgain%"=="Y" (goto CreateProgramGroup) else if /i "%createrAgain%"=="N" (goto launchProgramGroup)
+
+
+
+
+
+:debug
+echo Debug mode activated. Congratulations, you have achieved cool kid status.
+set debugMode=1
+goto line   
